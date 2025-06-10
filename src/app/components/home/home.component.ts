6ff22d9f8f3c301301bba21b335aa703f980f582
Mr Mark <markex133@gmail.com>
2025-06-10 12:22:42 +0200

    FrontEnd Update

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {AboutMeComponent} from "../about-me/about-me.component";
+import {ProyectosComponent} from "../proyectos/proyectos.component";
+import {ContactoComponent} from "../contacto/contacto.component";
+import {NavBarComponent} from "../nav-bar/nav-bar.component";
+import {HeaderComponent} from "../header/header.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    AboutMeComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    NavBarComponent,
+    HeaderComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
