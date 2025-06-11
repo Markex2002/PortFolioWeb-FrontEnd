76106453f245497762f25371bccf7f2a579a5c16
Mr Mark <markex133@gmail.com>
2025-06-11 13:27:44 +0200

    FrontEnd Update

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgParticlesModule} from "ng-particles";
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [
+    NgParticlesModule
+  ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-proyectos',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './proyectos.component.html',
   styleUrl: './proyectos.component.scss'
 })
 export class ProyectosComponent {
 
+  projects = [
+    {
+      title: 'RpgProyect',
+      description: 'A personal proyect I did to try and make a text based Rpg made purely on Java.',
+      image: 'assets/images/rpgImage.jpg',
+      github: 'https://github.com/Markex2002/ProyectoRpg2.0.git',
+      demo: ''
+    },
+    {
+      title: 'Final proyect app',
+      description: 'A full-stack website I did as my final proyect for my web developer degree, using Java, SpringBoot and Angular between others.',
+      image: 'assets/images/PortfolioProject.jpg',
+      github: 'https://github.com/Markex2002/ProyectoIntegrado.git',
+      demo: ''
+    },
+
+
+
+
+  ];
 }
